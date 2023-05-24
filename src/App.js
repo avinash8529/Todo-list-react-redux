1fd99c2a3f1fb9bcee811aa870d9165dfa3fd5ce
Avinash <avinash853002@gmail.com>
2023-05-24 09:47:34 +0530

    uploaded all

--- src/App.js
+++ src/App.js
@@ -1,22 +1,80 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AddTodoAction, RemoveTodoAction } from './actions/TodoActions';
 import './App.css';
 
 function App() {
+  const [todo, setTodo] = useState('');
+
+  const dispatch = useDispatch();
+
+  const Todo = useSelector((state) => state.Todo);
+  const { todos } = Todo;
+  //console.log(todos);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(AddTodoAction(todo));
+    //console.log(todo);
+    setTodo(''); // Clear the input field after submitting
+  };
+
+  const removeHandler = (event) => {
+    dispatch(RemoveTodoAction(event));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Todo list app in redux</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Enter a todo"
+            style={{
+              width: 350,
+              padding: 10,
+              borderRadius: 20,
+              border: 'none',
+              fontSize: 20,
+            }}
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <button
+            style={{
+              padding: 12,
+              borderRadius: 25,
+              fontSize: 15,
+              marginLeft: 20,
+            }}
+            type="submit"
+          >
+            Go
+          </button>
+        </form>
+        <ul className="allTodos">
+          {todos.map((todo) => {
+            const {id, todo:task} =todo;
+            return (
+            <li key={id} className="singleTodo">
+              <span className="todoText">{task}</span>
+              <button
+                style={{
+                  borderRadius: 25,
+                  padding: 10,
+                  border: '1px solid white',
+                  color: 'white',
+                  backgroundColor: 'orangered',
+                  marginLeft: '10px',
+                }}
+                onClick={() => removeHandler(task)}
+              >
+                Delete
+              </button>
+            </li>
+          )})}
+        </ul>
       </header>
     </div>
   );
